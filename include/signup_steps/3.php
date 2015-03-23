92c2f5e517022b86a27010bbda55edf7a1dc4b55
pooya <pooya_alen1990@yahoo.com>
2015-03-23 16:00:44 +0430

    ok
    lkm

---
+++ include/signup_steps/3.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['go_register'])){
+	
+	$_SESSION['signup_users'] = 4;
+	header('Location: ?page=signup');
+	}
+
+?>
+<form method="post" role="form" class="form-horizontal" dir="rtl">
+   form e 2
+</form>
