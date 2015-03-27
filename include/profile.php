914abedc7ee30b9b26a57fb0ba7050267cbfff7e
mehrdad-parsa68 <mehrdad.parsa68@gmail.com>
2015-03-27 19:07:13 +0330

    profile start

--- include/profile.php
+++ include/profile.php
@@ -41,54 +41,12 @@
         </div>
     </div>
     
-	<div class="col-md-8 contact">
-    	<div class="col-xs-12">
-        	<h3>ارتباط با ما</h3>
-            <hr class="hr-beauty">
-        	<div class="col-xs-6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3238.84057774771!2d51.4248165!3d35.7301391!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQzJzQ5LjkiTiA1McKwMjUnMjkuOCJF!5e0!3m2!1sen!2sir!4v1420010909785" height="400" frameborder="0" style="border:0"></iframe>
-            </div>
-            <div class="col-xs-6">
-           		<h4>آدرس :</h4>
-                
-                <p>خیابان شهید بهشتی ، بعد از خیابان میرعماد، پلاک 294 ، طبقه پنجم ، واحد 501</p>
-                <hr>
-                <h4>تلفن :</h4>
-                
-                <p>88759591 - 021</p>
-                <hr>
-                <h4>خط ویژه :</h4>
-                
-                <p>88507922 - 021</p>
-                <hr>
-                <h4>ایمیل :</h4>
-                
-                <p>info@sarekar.ir</p>
-            </div>
-            <div class="col-xs-12">
-            	<legend>ارتباط با مدیریت</legend>
-           		 <form id="contact-form" role="form" method="post">
-                 
-            <div class="form-group name">
-                <label for="name">نام و نام خانوادگی</label>
-                <input type="text" class="form-control" id="name" name="name" required>
-            </div>
-            <div class="form-group name">
-                <label for="name">تلفن</label>
-                <input type="text" class="form-control" id="phone" name="mobile" required>
-            </div>
-            <div class="form-group email">
-                <label for="email">ایمیل</label>
-                <input type="email" class="form-control" id="mail" name="mail"  required>
-            </div>        
-            <div class="form-group message">
-                <label for="message">متن پیام</label>
-                <textarea class="form-control" rows="6" id="content" name="message"  required></textarea>
-            </div>
-            <input name="submit" type="submit" value="ارسال پیام" class="btn btn-warining submit margin-bottom-20">
-        </form>
-            </div>
-    	
-    </div>
-</div>
+	<div class="col-md-8 profile">
+    	<div class="col-sm-3">
+        	<img src="images/profile.png" class="img-responsive" style="margin:auto">
+        </div>
+        <div class="col-sm-9">
+        	<h2>مهرداد پارسا</h2>
+        </div>
+        <div class="clearfix"></div>
 </div>
