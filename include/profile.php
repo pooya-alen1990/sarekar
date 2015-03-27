52bb497925ff22d2060e5e1f31a869e2ca488185
mehrdad-parsa68 <mehrdad.parsa68@gmail.com>
2015-03-27 19:48:43 +0330

    profile again

--- include/profile.php
+++ include/profile.php
@@ -42,11 +42,39 @@
     </div>
     
 	<div class="col-md-8 profile">
-    	<div class="col-sm-3">
+    	<div class="col-sm-3" dir="ltr">
         	<img src="images/profile.png" class="img-responsive" style="margin:auto">
+            <div  class="upload">
+            <form method="post" enctype="multipart/form-data">
+            	<input type="file">
+            </form>
+            </div>
         </div>
         <div class="col-sm-9">
         	<h2>مهرداد پارسا</h2>
         </div>
         <div class="clearfix"></div>
-</div>
+        <div class="row">
+        <h3 class="title">اطلاعات شخصی</h3>
+        <div class="col-sm-2 text-center">
+        	<a href="#" class="edit">ویرایش اطلاعات</a>
+        </div>
+        <div class="col-sm-5">
+        	<h4>وضعیت تاهل :</h4>
+            <h4>وضعیت سربازی :</h4>	
+        </div>
+        <div class="col-sm-5">
+          <h4>نام و نام خانوادگی : </h4>
+          <h4>کد ملی :</h4>
+          <h4>مذهب :</h4>
+          <h4>جنسیت :</h4>
+          
+        </div>
+        </div>
+        <div class="row">
+        <h3 class="title">اطلاعات تحصیلی</h3>
+        </div>
+        <div class="row">
+        <h3 class="title">اطلاعات شغلی</h3>
+        </div>
+</div></div>
