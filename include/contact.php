71828918369ee5f044643c0d206606c3ecf70aec
pooya <pooya_alen1990@yahoo.com>
2015-03-28 15:31:57 +0430

    ok

--- include/contact.php
+++ include/contact.php
@@ -1,45 +1,5 @@
 <div class="home">
-	<div class="col-md-4">
-    	<div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title text-center">آخرین کارجویان</h3>
-          </div>
-          <div class="panel-body last-user">
-            <p><i class="fa fa-male"></i> اصغر فرهادی </p><p align="left">2 فروردین 1393</p>
-            <p><i class="fa fa-female"></i> لیلا حاتمی </p><p align="left">6 دی 1923</p>
-            <p><i class="fa fa-male"></i> مهرداد پارسا </p><p align="left">110</p>
-            <p><i class="fa fa-female"></i> پویا صبرآموز </p><p align="left">110</p>
-          </div>
-        </div>
-        <div>
-        <img src="images/sidebar1.jpg" class="img-responsive">
-        </div>
-        
-        <div class="panel panel-default stat">
-          <div class="panel-heading">
-            <h3 class="panel-title text-center">آمار سایت</h3>
-            
-          </div>
-          <div class="panel-body">
-            <p><i class="fa fa-arrow-left"></i> تعداد کارجویان : </p><p align="left">110</p>
-            <p><i class="fa fa-arrow-left"></i> تعداد کارفرمایان :  </p><p align="left">1545</p>
-            <p><i class="fa fa-arrow-left"></i> تعداد فرصت های شغلی :  </p><p align="left">48794</p>
-            <p><i class="fa fa-arrow-left"></i> استخدام از طریق سایت :  </p><p align="left">215120</p>
-          </div>
-           <div class="panel-heading no-redius">
-            <h3 class="panel-title text-center">آمار بازدید کنندگان</h3>
-            
-          </div>
-          <div class="panel-body">
-            <p><i class="fa fa-arrow-left"></i> تعداد افراد آنلاین :</p><p align="left">110</p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کنندگان امروز :  </p><p align="left">1545</p>
-            <p><i class="fa fa-arrow-left"></i> بازدیدکنندگان این هفته :  </p><p align="left">48794</p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کنندگان این ماه :  </p><p align="left">215120</p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کنندگان این سال :  </p><p align="left">215120</p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کل :  </p><p align="left">215120</p>
-          </div>
-        </div>
-    </div>
+	<?php include 'left_nav.php'; ?>
     
 	<div class="col-md-8 contact">
     	<div class="col-xs-12">
--- include/home.php
+++ include/home.php
@@ -1,45 +1,5 @@
 <div class="home">
-	<div class="col-md-4">
-    	<div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title text-center">آخرین کارجویان</h3>
-          </div>
-          <div class="panel-body last-user">
-            <p><i class="fa fa-male"></i><a href="#"> اصغر فرهادی </a></p><p align="left">2 فروردین 1393</p>
-            <p><i class="fa fa-female"></i><a href="#"> لیلا حاتمی </a></p><p align="left">6 دی 1923</p>
-            <p><i class="fa fa-male"></i><a href="#"> مهرداد پارسا </a></p><p align="left">110</p>
-            <p><i class="fa fa-female"></i><a href="#"> پویا صبرآموز </a></p><p align="left">110</p>
-          </div>
-        </div>
-        <div>
-        <img src="images/sidebar1.jpg" class="img-responsive">
-        </div>
-        
-        <div class="panel panel-default stat">
-          <div class="panel-heading">
-            <h2 class="panel-title text-center">آمار سایت</h2>
-            
-          </div>
-          <div class="panel-body">
-          	<h4 class="text-center">اعضا</h4>
-            <hr class="hr-light">
-            <p><i class="fa fa-arrow-left"></i> تعداد کارجویان : </p><p align="left">110</p>
-            <p><i class="fa fa-arrow-left"></i> تعداد کارفرمایان :  </p><p align="left">1545</p>
-            <p><i class="fa fa-arrow-left"></i> تعداد فرصت های شغلی :  </p><p align="left">48794</p>
-            <p><i class="fa fa-arrow-left"></i> استخدام از طریق سایت :  </p><p align="left">215120</p>
-          </div>
-           <h4 class="text-center">وب سایت</h4>
-            <hr class="hr-light">
-          <div class="panel-body">
-            <p><i class="fa fa-arrow-left"></i> تعداد افراد آنلاین :</p><p align="left"><?php echo $online; ?></p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کنندگان امروز :  </p><p align="left"><?php echo $day; ?></p>
-            <p><i class="fa fa-arrow-left"></i> بازدیدکنندگان این هفته :  </p><p align="left"><?php echo $week; ?></p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کنندگان این ماه :  </p><p align="left"><?php echo $month; ?></p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کنندگان این سال :  </p><p align="left"><?php echo $year; ?></p>
-            <p><i class="fa fa-arrow-left"></i> بازدید کل :  </p><p align="left"><?php echo $all; ?></p>
-          </div>
-        </div>
-    </div>
+	<?php include 'left_nav.php'; ?>
     
 	<div class="col-md-8">
     	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
